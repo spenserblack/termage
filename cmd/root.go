9184f57b018999566a921becbd586bd4d70a71fb
Spenser Black <spenserblack01@gmail.com>
2021-04-20 11:42:24 -0400

    Move UI code out of cmd configuration

--- cmd/root.go
+++ cmd/root.go
@@ -1,53 +1,26 @@
 package cmd
 
 import (
-	"errors"
 	"fmt"
 	"os"
 
-	ui "github.com/gizak/termui/v3"
-	"github.com/gizak/termui/v3/widgets"
 	"github.com/spf13/cobra"
-	"golang.org/x/term"
 )
 
 var (
-	imageFile string
+	// ImageFile is the filepath to the initial file/directory.
+	ImageFile string
 	rootCmd   = &cobra.Command{
 		Use:   "asciiimage <FILE | DIRECTORY>",
 		Short: "Browse image files as ASCII in your terminal",
 		Long: `This application is a tool to view your image files
 as ASCII art without leaving your terminal`,
 		Args: cobra.ExactArgs(1),
-		RunE: rootRun,
+		Run: func(cmd *cobra.Command, args []string) {
+			ImageFile = args[0]
+		},
 	}
-	errNotATerminal = errors.New("Please run in an environment that can initialize a TUI")
 )
-
-func rootRun(cmd *cobra.Command, args []string) error {
-	width, height, err := term.GetSize(int(os.Stdin.Fd()))
-	if err != nil {
-		return errNotATerminal
-	}
-	if err := ui.Init(); err != nil {
-		return errNotATerminal
-	}
-	defer ui.Close()
-
-	p := widgets.NewParagraph()
-	p.Border = false
-	p.SetRect(0, 0, width, height)
-	p.Text = fmt.Sprintf("File: %v\nInner bounds: %v", args[0], p.Inner)
-
-	ui.Render(p)
-
-	for e := range ui.PollEvents() {
-		if e.Type == ui.KeyboardEvent {
-			break
-		}
-	}
-	return nil
-}
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "github.com/spenserblack/asciiimage/cmd"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+	"golang.org/x/term"
+
+	"github.com/spenserblack/asciiimage/cmd"
+)
 
 func main() {
 	cmd.Execute()
+
+	width, height, err := term.GetSize(int(os.Stdin.Fd()))
+	if err != nil {
+		log.Fatalf("Couldn't get terminal size: %v", err)
+	}
+	if err := ui.Init(); err != nil {
+		log.Fatalf("Couldn't initialize UI: %v", err)
+	}
+	defer ui.Close()
+
+	p := widgets.NewParagraph()
+	p.Border = false
+	p.SetRect(0, 0, width, height)
+	p.Text = fmt.Sprintf("File: %v\nInner bounds: %v", cmd.ImageFile, p.Inner)
+
+	ui.Render(p)
+
+	for e := range ui.PollEvents() {
+		if e.Type == ui.KeyboardEvent {
+			break
+		}
+	}
 }
