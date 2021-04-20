73e09e70785cca7931292fa7b77ffb0f5670f23e
Spenser Black <spenserblack01@gmail.com>
2021-04-20 11:35:57 -0400

    Draw paragraph with bounds of terminal

--- cmd/root.go
+++ cmd/root.go
@@ -8,6 +8,7 @@ import (
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
 	"github.com/spf13/cobra"
+	"golang.org/x/term"
 )
 
 var (
@@ -20,18 +21,23 @@ as ASCII art without leaving your terminal`,
 		Args: cobra.ExactArgs(1),
 		RunE: rootRun,
 	}
+	errNotATerminal = errors.New("Please run in an environment that can initialize a TUI")
 )
 
 func rootRun(cmd *cobra.Command, args []string) error {
-	fmt.Println("file: ", args[0])
+	width, height, err := term.GetSize(int(os.Stdin.Fd()))
+	if err != nil {
+		return errNotATerminal
+	}
 	if err := ui.Init(); err != nil {
-		return errors.New("Please run in an environment that can initialize a TUI")
+		return errNotATerminal
 	}
 	defer ui.Close()
 
 	p := widgets.NewParagraph()
-	p.Text = fmt.Sprintf("File: %v", args[0])
-	p.SetRect(0, 0, 25, 5)
+	p.Border = false
+	p.SetRect(0, 0, width, height)
+	p.Text = fmt.Sprintf("File: %v\nInner bounds: %v", args[0], p.Inner)
 
 	ui.Render(p)
 
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/gizak/termui/v3 v3.1.0
 	github.com/spf13/cobra v1.1.3
+	golang.org/x/term v0.0.0-20210406210042-72f3dc4e9b72
 )
