9d74cb194ec636bece2d11c907e463f9a5b2f29e
Spenser Black <spenserblack01@gmail.com>
2021-04-20 10:42:50 -0400

    Add TUI

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,12 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
 	"github.com/spf13/cobra"
 )
 
@@ -15,11 +18,30 @@ var (
 		Long: `This application is a tool to view your image files
 as ASCII art without leaving your terminal`,
 		Args: cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("file: ", args[0])
-		},
+		RunE: rootRun,
 	}
 )
+
+func rootRun(cmd *cobra.Command, args []string) error {
+	fmt.Println("file: ", args[0])
+	if err := ui.Init(); err != nil {
+		return errors.New("Please run in an environment that can initialize a TUI")
+	}
+	defer ui.Close()
+
+	p := widgets.NewParagraph()
+	p.Text = fmt.Sprintf("File: %v", args[0])
+	p.SetRect(0, 0, 25, 5)
+
+	ui.Render(p)
+
+	for e := range ui.PollEvents() {
+		if e.Type == ui.KeyboardEvent {
+			break
+		}
+	}
+	return nil
+}
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/spenserblack/asciiimage
 
 go 1.16
 
-require github.com/spf13/cobra v1.1.3
+require (
+	github.com/gizak/termui/v3 v3.1.0
+	github.com/spf13/cobra v1.1.3
+)
