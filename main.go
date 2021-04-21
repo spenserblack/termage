33aeb6d04ae7df52bb8a7d2aded84274489ab0ec
Spenser Black <spenserblack01@gmail.com>
2021-04-21 12:07:30 -0400

    Display contents in a flex box

--- main.go
+++ main.go
@@ -18,8 +18,20 @@ func main() {
 		SetChangedFunc(func() {
 			app.Draw()
 		})
+	titleView := tview.NewTextView().
+		SetText(cmd.ImageFile).
+		SetTextAlign(tview.AlignCenter)
+	footerView := tview.NewTextView().
+		SetText("This is the footer").
+		SetTextAlign(tview.AlignCenter)
 
-	if err := app.SetRoot(imageView, true).SetFocus(imageView).Run(); err != nil {
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(titleView, 0, 10, false).
+		AddItem(imageView, 0, 80, false).
+		AddItem(footerView, 0, 10, false)
+
+	if err := app.SetRoot(flex, true).SetFocus(flex).Run(); err != nil {
 		log.Fatal(err)
 	}
 }
