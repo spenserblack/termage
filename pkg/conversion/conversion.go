526145698dc80fc76843b005b317115ea3cb0a4d
Spenser Black <spenserblack01@gmail.com>
2021-04-21 15:49:47 -0400

    Add conversion of image to colored runes

--- pkg/conversion/conversion.go
+++ pkg/conversion/conversion.go
@@ -1,6 +1,9 @@
 package conversion
 
-import "image/color"
+import (
+	"image"
+	"image/color"
+)
 
 // AlphaChars contains runes representing alpha levels, from most transparent
 // (lowest index) to most opaque (highest index).
@@ -18,6 +21,12 @@ type RGBRune struct {
 	char    rune
 }
 
+// RGBRunes is a helper type for a slice of RGBRunes.
+type RGBRunes struct {
+	rgbRunes      []RGBRune
+	width, height int
+}
+
 // RGBRuneFromColor converts a color into an RGBRune.
 func RGBRuneFromColor(c color.Color) RGBRune {
 	r, g, b, a := c.RGBA()
@@ -28,4 +37,40 @@ func RGBRuneFromColor(c color.Color) RGBRune {
 		b,
 		AlphaChars[int(a/(^uint32(0)/uint32(len(AlphaChars))))],
 	}
+}
+
+// RGBRunesFromImage creates a slice of RGBRunes from an Image.
+func RGBRunesFromImage(i image.Image) RGBRunes {
+	bounds := i.Bounds()
+	width := bounds.Max.X
+	height := bounds.Max.Y
+
+	rgbRunes := make([]RGBRune, 0, width*height)
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			rgbRunes = append(rgbRunes, RGBRuneFromColor(i.At(x, y)))
+		}
+	}
+
+	return RGBRunes{
+		rgbRunes,
+		width,
+		height,
+	}
+}
+
+// At gets the RGBRune at a point.
+func (rgb *RGBRunes) At(x, y int) RGBRune {
+	return rgb.rgbRunes[y*rgb.width+x]
+}
+
+// Width gets the width of the image as colored runes.
+func (rgb *RGBRunes) Width() int {
+	return rgb.width
+}
+
+// Height gets the height of the image as colored runes.
+func (rgb *RGBRunes) Height() int {
+	return rgb.height
 }
