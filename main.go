beda94c1435d2c29042fb7a828e2937527409ab6
Spenser Black <spenserblack01@gmail.com>
2021-04-21 11:49:00 -0400

    Render text view

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 
-	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 
 	"github.com/spenserblack/termage/cmd"
@@ -12,11 +11,15 @@ import (
 func main() {
 	cmd.Execute()
 
-	rootBox := tview.NewBox().
-		SetBorder(true).
-		SetTitle(cmd.ImageFile).
-		SetBackgroundColor(tcell.ColorDefault)
-	if err := tview.NewApplication().SetRoot(rootBox, true).Run(); err != nil {
+	app := tview.NewApplication()
+	imageView := tview.NewTextView().
+		SetDynamicColors(true).
+		SetText("Image will be drawn here.\nIt can be zoomed into and scrolled.").
+		SetChangedFunc(func() {
+			app.Draw()
+		})
+
+	if err := app.SetRoot(imageView, true).SetFocus(imageView).Run(); err != nil {
 		log.Fatal(err)
 	}
 }
