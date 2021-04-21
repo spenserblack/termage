09e57f71fd6549c47f995293fcc5809efd38fa1a
Spenser Black <spenserblack01@gmail.com>
2021-04-21 11:27:12 -0400

    Use tview to draw UI

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/spenserblack/termage
 go 1.16
 
 require (
-	github.com/gizak/termui/v3 v3.1.0
+	github.com/gdamore/tcell/v2 v2.2.1
+	github.com/rivo/tview v0.0.0-20210312174852-ae9464cc3598
 	github.com/spf13/cobra v1.1.3
-	golang.org/x/term v0.0.0-20210406210042-72f3dc4e9b72
 )
--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"image"
-	_ "image/jpeg"
 	"log"
-	"os"
 
-	ui "github.com/gizak/termui/v3"
-	"github.com/gizak/termui/v3/widgets"
-	"golang.org/x/term"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 
 	"github.com/spenserblack/termage/cmd"
 )
@@ -16,36 +12,11 @@ import (
 func main() {
 	cmd.Execute()
 
-	reader, err := os.Open(cmd.ImageFile)
-	if err != nil {
-		log.Fatalf("Couldn't open image file: %v", err)
-	}
-	defer reader.Close()
-
-	im, format, err := image.Decode(reader)
-	_ = format
-	if err != nil {
-		log.Fatalf("Couldn't decode image: %v", err)
-	}
-
-	width, height, err := term.GetSize(int(os.Stdin.Fd()))
-	if err != nil {
-		log.Fatalf("Couldn't get terminal size: %v", err)
-	}
-	if err := ui.Init(); err != nil {
-		log.Fatalf("Couldn't initialize UI: %v", err)
-	}
-	defer ui.Close()
-
-	i := widgets.NewImage(im)
-	i.Border = false
-	i.SetRect(0, 0, width, height)
-
-	ui.Render(i)
-
-	for e := range ui.PollEvents() {
-		if e.Type == ui.KeyboardEvent {
-			break
-		}
+	rootBox := tview.NewBox().
+		SetBorder(true).
+		SetTitle(cmd.ImageFile).
+		SetBackgroundColor(tcell.ColorDefault)
+	if err := tview.NewApplication().SetRoot(rootBox, true).Run(); err != nil {
+		log.Fatal(err)
 	}
 }
