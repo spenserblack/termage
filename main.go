43fd59c4fa8674ec1ac763394f01481935703a6f
Spenser Black <spenserblack01@gmail.com>
2021-04-26 13:17:20 -0400

    Fix regression: resize image on screen resize

--- main.go
+++ main.go
@@ -90,7 +90,7 @@ func main() {
 	for {
 		switch ev := s.PollEvent().(type) {
 		case *tcell.EventResize:
-			s.Sync()
+			resizedImage = resizeImageToTerm(originalImage, s)
 			drawImage()
 		case *tcell.EventKey:
 			switch ev.Key() {
