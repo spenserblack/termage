ea6a7d93b04907db7a971289cd9109307992c7c9
Spenser Black <spenserblack01@gmail.com>
2021-06-16 09:25:11 -0400

    Use struct to define controls to be printed

--- cmd/controls.go
+++ cmd/controls.go
@@ -6,29 +6,38 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var controlText = []string{
-	"n - Next image",
-	"N - Previous image",
-	"z - Increase zoom by 10 percentiles",
-	"Z - Decrease zoom by 10 percentiles",
-	"f - Fit to screen",
-	"h - Scroll left one pixel",
-	"H - Scroll left 10%",
-	"j - Scroll down one pixel",
-	"J - Scroll down 10%",
-	"k - Scroll up one pixel",
-	"K - Scroll up 10%",
-	"l - Scroll right one pixel",
-	"L - Scroll right 10%",
-	"Esc - Exit application",
+type controlMapping struct {
+	key    string
+	action string
+}
+
+func (m controlMapping) String() string {
+	return fmt.Sprintf("%s - %s", m.key, m.action)
+}
+
+var controls = []controlMapping{
+	controlMapping{"n", "Next image"},
+	controlMapping{"N", "Previous image"},
+	controlMapping{"z", "Increase zoom by 10 percentiles"},
+	controlMapping{"Z", "Decrease zoom by 10 percentiles"},
+	controlMapping{"f", "Fit to screen"},
+	controlMapping{"h", "Scroll left one pixel"},
+	controlMapping{"H", "Scroll left 10%"},
+	controlMapping{"j", "Scroll down one pixel"},
+	controlMapping{"J", "Scroll down 10%"},
+	controlMapping{"k", "Scroll up one pixel"},
+	controlMapping{"K", "Scroll up 10%"},
+	controlMapping{"l", "Scroll right one pixel"},
+	controlMapping{"L", "Scroll right 10%"},
+	controlMapping{"Esc", "Exit application"},
 }
 
 var controlsCmd = &cobra.Command{
 	Use:   "controls",
 	Short: "Print controls",
 	Run: func(cmd *cobra.Command, args []string) {
-		for _, message := range controlText {
-			fmt.Println(message)
+		for _, control := range controls {
+			fmt.Println(control)
 		}
 	},
 }
