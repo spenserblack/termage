6786070ae5dc412f5c02864a4c64017a83773f8e
Spenser Black <spenserblack01@gmail.com>
2021-11-09 09:31:46 -0500

    Download release artifact with `install.sh`
    Drops requirement for go to be installed on user's system if the user is
    using Linux or MacOS.

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
-TEMP=$(mktemp -d)
-git clone https://github.com/spenserblack/termage.git $TEMP
-cd $TEMP
-make install
-cd -
-echo Removing $TEMP
-rm -rf $TEMP
+#!/bin/bash
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	PLATFORM="ubuntu"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	PLATFORM="macos"
+else
+	echo "Unsupported OS type: $OSTYPE" >&2
+	exit 1
+fi
+
+sudo wget -O /usr/local/bin/termage "https://github.com/spenserblack/termage/releases/latest/download/termage-$PLATFORM"
